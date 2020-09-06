a4f151065b228d29cc299934c65238dcc11ae169
Remco Verhoef <remco@dutchcoders.io>
2020-09-06 15:04:04 +0200

    bumped v0.1.5

--- Formula/cloudman.rb
+++ Formula/cloudman.rb
@@ -1,15 +1,15 @@
 class Cloudman < Formula
   desc "Terminal user interface for managing Amazon EC2 instances"
   homepage "https://github.com/dutchcoders/cloudman"
-  version "0.1.4"
+  version "0.1.5"
   
   if OS.mac?
     url "https://github.com/dutchcoders/cloudman/releases/download/#{version}/cloudman-#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "c3fbb9c8c7e086050d32c7a1db2a32cd86cf1238b33cfe581fb45d65133f7bb8"
+    sha256 "0e732eeeb71efb5fd0cca68704192d20533cbb6200515380846bd055354eaccb"
   elsif OS.linux?
     if Hardware:: CPU.intel?
       url "https://github.com/dutchcoders/cloudman/releases/download/#{version}/cloudman-#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "2e17185c7226d690cba5eb976b2f8e3a7ffcc444d460a0b1e606d2f518e3a142"
+      sha256 "da58f42a31d4b1d9d96ee2421f67c2db3657a9e22e602e0de3d9e76d20ffa964"
     end
   end
 
